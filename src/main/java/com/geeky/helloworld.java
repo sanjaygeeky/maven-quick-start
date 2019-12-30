1edca466143c8759b3369ec197571491734dfffe
sanjaygeeky <sanjaygeeky@gmail.com>
2019-12-30 09:50:32 +0530

    Adding the target file for deployment

---
+++ src/main/java/com/geeky/helloworld.java
@@ -0,0 +1,11 @@
+package com.geeky;
+
+public class helloworld 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, World"); 
+    } 
+} 
